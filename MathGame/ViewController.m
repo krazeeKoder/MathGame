882b846e4886aef4cbd8c21582c6d5aea45115aa
KrazeeKoder <tony.tulai@gmail.com>
2016-01-18 15:50:27 -0500

    Empty project autolayout/size classes disabled

---
+++ MathGame/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MathGame
+//
+//  Created by Anthony Tulai on 2016-01-18.
+//  Copyright © 2016 Anthony Tulai. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
